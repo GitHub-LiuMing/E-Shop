19ffc8a9a4601c7dd0c81c304ea4944e5ee1714e
GitHub-LiuMing <GoogLiuMing@Gmail.com>
2019-12-13 18:22:41 +0800

    备份

---
+++ src/main/java/com/liuming/eshop/controller/memberController/MemberController.java
@@ -0,0 +1,15 @@
+package com.liuming.eshop.controller.memberController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 会员
+ * @ClassName MemberController
+ * @Author 鲸落
+ * @date 2019.12.12 15:00
+ */
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+}
---
+++ src/main/java/com/liuming/eshop/service/memberService/impl/MemberServiceImpl.java
@@ -0,0 +1,12 @@
+package com.liuming.eshop.service.memberService.impl;
+
+import com.liuming.eshop.service.memberService.MemberService;
+
+/**
+ * @Description 
+ * @ClassName MemberServiceImpl
+ * @Author 鲸落
+ * @date 2019.12.12 15:01
+ */
+public class MemberServiceImpl implements MemberService {
+}
